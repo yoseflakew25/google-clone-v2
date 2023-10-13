45f27e9f4a8c857356251dae2fc4bf020575072d
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-10-13 15:04:10 +0300

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,23 @@
 import Image from 'next/image'
 import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
+
+
 export default function Home() {
 
   return (
     <>
       <HomeHeader />
-
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="/google_PNG.png"
+          alt='homepage google pic'
+          priority={true}
+        />
+        <HomeSearch />
+      </div>
     </>
-  )
+  );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
-import CountryLookup from "./CountryLookup";
 
 export default function Footer() {
   return (
     <footer className="absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full">
       <div className="border-b px-8 py-3">
-        <CountryLookup />
+        Ethiopia
       </div>
       <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
         <ul className="flex items-center space-x-6">
