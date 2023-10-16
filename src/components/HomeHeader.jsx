54fc0613016d757f27a21f5e98cb919dcb08be9b
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-10-16 14:03:32 +0300

    Update HomeHeader.jsx

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -12,13 +12,13 @@ export default function HomeHeader() {
           Images
         </Link>
         <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
-        <a href='https://github.com/yoseflakew25/google-clone-v2' target="_blank" className/* In the code snippet provided, `="` is used as an attribute value in JSX
+        <Link href='https://github.com/yoseflakew25/google-clone-v2' target="_blank" className/* In the code snippet provided, `="` is used as an attribute value in JSX
         syntax. It is used to assign a value to an attribute of an HTML element. In
         this case, it is used to assign the URL to the `href` attribute of the
         `Link` component. */
         ="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">
           Visit Github
-        </a>
+        </Link>
       </div>
     </header>
   );
